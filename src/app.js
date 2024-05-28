7ac9865282fa0a533d73eb1655d5ca1db1ad5ca5
Babita00 <babitaasth07@gmail.com>
2024-05-28 12:25:50 +0545

    route added

--- src/app.js
+++ src/app.js
@@ -12,4 +12,7 @@ app.use(express.json({ limit: "16kb;" }));
 app.use(express.urlencoded);
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import UserRoute from "./routes/user.routes.js";
+app.use("/api/v1/users", UserRoute);
 export { app };
