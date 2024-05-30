87c0bfb4d20eefce114487ab0531502e613e1a75
Babita00 <babitaasth07@gmail.com>
2024-05-30 21:30:08 +0545

    jwt update effect

--- src/app.js
+++ src/app.js
@@ -1,20 +1,28 @@
+// src/app.js
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
+import { authenticateJWT } from "./middleware/jwtauth.middleware.js";
+import UserRoute from "./routes/user.routes.js";
 
 const app = express();
 
+// Middleware configuration
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   }),
 );
-app.use(express.json({ limit: "16kb;" }));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cookieParser());
 
-import UserRoute from "./routes/user.routes.js";
+// Example route for user login (handled in user.routes.js)
 app.use("/api/v1/users", UserRoute);
+
+// Protected route example
+app.get("/api/v1/protected", authenticateJWT, (req, res) => {
+  res.send("This is a protected route");
+});
+
 export { app };
