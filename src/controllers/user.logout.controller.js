ebcf4cbdae020baf0f94fc71d5ca60ee9ea00660
Babita00 <babitaasth07@gmail.com>
2024-05-31 15:21:56 +0545

    logout added

---
+++ src/controllers/user.logout.controller.js
@@ -0,0 +1,24 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+//logout user
+
+const logoutUser = asyncHandler(async (req, res) => {
+  // Invalidate tokens on the server side (optional)
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1, // this removes the field from document
+      },
+    },
+    {
+      new: true,
+    },
+  );
+
+  // Respond to the client
+  return res.status(200).json(new ApiResponse(200, {}, "User logged out"));
+});
+export { logoutUser };
