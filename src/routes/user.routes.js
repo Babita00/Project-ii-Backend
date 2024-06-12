5c6d5cb94913641be21fc4fef741d787e3a85a43
Babita00 <babitaasth07@gmail.com>
2024-06-12 14:46:43 +0545

    update in user route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,11 @@ import { loginUser } from "../controllers/user.login.controller.js";
 import { logoutUser } from "../controllers/user.logout.controller.js";
 import { verifyJWT } from "../middleware/jwtauth.middleware.js";
 import { changeCurrentPassword } from "../controllers/user.changePassword.js";
+import {
+  forgotPassword,
+  resetPassword,
+} from "../controllers/forgotPassword.controller.js";
+
 const router = Router();
 
 // Register route
@@ -18,5 +23,9 @@ router.route("/logout").post(verifyJWT, logoutUser);
 
 //change password
 router.route("/passwordchange").post(verifyJWT, changeCurrentPassword);
+
+//for forgot password and reset password
+router.route("/forgotpassword").post(forgotPassword);
+router.route("/passwordreset/:resetToken").put(resetPassword);
 
 export default router;
