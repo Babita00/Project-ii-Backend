1e9f90fbea87458d38854bc86cc386b1c75124ce
Babita00 <babitaasth07@gmail.com>
2024-07-25 17:56:51 +0545

    DELETE AND PUT operation added in methos of cors

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   },
 });
